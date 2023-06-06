d58d33b5f123aabb9676dcc2342e5f41c7648105
decitre <590094+decitre@users.noreply.github.com>
2023-06-06 17:52:34 +0200

    Add a deprecation notice and bump-up to 0.2.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     description="Flexible Polyline encoding (PBAPI extension)",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version="0.2.0",
+    version="0.2.1",
     author="decitre",
     packages=["flexpolyline_pbapi"],
     # SPDX-License-Identifier: MIT
